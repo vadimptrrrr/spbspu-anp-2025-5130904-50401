a7a688e4e9d8120fffbb996ced445d926c0c7f16
Petrov Vadim <vadikdydy@mail.ru>
2025-11-10 08:23:09 +0300

    foo for add matrix

--- petrov.vadim/P3/main.cpp
+++ petrov.vadim/P3/main.cpp
@@ -1,4 +1,22 @@
-int main()
-{
+#include <iostream>
+#include <fstream>
+#include <cstddef>
 
+namespace petrov
+{
+  void create(std::istream & input, int * mtx, size_t rows, size_t cols);
+} 
+
+void create(std::istream & input, int * mtx, size_t rows, size_t cols)
+{
+  for(size_t i = 0; i < (rows*cols); ++i)
+  {
+    input >> mtx[i];
+  }
+}
+
+int main(int argc, char ** argv)
+{
+  
+  
 }
